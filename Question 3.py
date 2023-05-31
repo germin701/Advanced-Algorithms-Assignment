f8445c70adaab8eade80507ad3e1d7caae6f393c
germin701 <104176182+germin701@users.noreply.github.com>
2023-05-31 14:49:01 +0800

    Update Question 3.py

--- Question 3.py
+++ Question 3.py
@@ -6,28 +6,52 @@ class Graph:
         self.graph[vertex] = {}
 
     def add_edge(self, start, end, weight):
+        # Add vertex if not exist
         if start not in self.graph:
             self.add_vertex(start)
+        # Add vertex if not exist
         if end not in self.graph:
             self.add_vertex(end)
+        # Add weight to the dictionary
         self.graph[start][end] = weight
 
+    def print(self):
+        for start in self.graph:
+            for end, weight in self.graph[start].items():
+                print(start, "-", weight, "->", end)
+
     def listAdjacentVertex(self, vertex):
+        # Verify the vertex is existed or not
         if vertex in self.graph:
-            for v in self.graph[vertex]:
-                print(v)
+            # Verify the vertex has adjacent vertex or not
+            if self.graph[vertex]:
+                # Print the key in the dictionary which are adjacent of the vertex
+                for v in self.graph[vertex]:
+                    print(v)
+            else:
+                print("None")
         else:
             return "Vertex not found"
 
     def sumHighestAdjacentVertex(self, vertex):
+        # The sum of weight
         value = 0
+        # Verify the vertex is existed or not
         if vertex in self.graph:
+            # Sum up the value in the dictionary which are the weight of the adjacent vertex
             for v in self.graph[vertex]:
                 value += self.graph[vertex][v]
             return value
         else:
             return value
 
+    def print_adjacent_details(self):
+        # Loop through all the vertex to print out their adjacent vertices and the sum of their adjacent vertices
+        for vertex in self.graph:
+            print("\nAdjacent vertices of", str(vertex), ": ")
+            self.listAdjacentVertex(vertex)
+            print("Sum of weights of the adjacent vertices of", str(vertex), ":",
+                  self.sumHighestAdjacentVertex(vertex))
 
 # Graph 1
 graph1 = Graph()
@@ -38,9 +62,9 @@ graph1.add_edge('B', 'D', 10)
 graph1.add_edge('C', 'E', 3)
 graph1.add_edge('D', 'F', 11)
 graph1.add_edge('E', 'D', 4)
-print("Graph 1\nAdjacent vertices of A: ")
-graph1.listAdjacentVertex('A')
-print("Sum of weights for the vertex A:", graph1.sumHighestAdjacentVertex('A'))
+print("Graph 1")
+graph1.print()
+graph1.print_adjacent_details()
 
 # Graph 2
 graph2 = Graph()
@@ -54,6 +78,6 @@ graph2.add_edge('K', 'H', 4)
 graph2.add_edge('K', 'J', 5)
 graph2.add_edge('K', 'L', 1)
 graph2.add_edge('L', 'G', 5)
-print("\nGraph 2\nAdjacent vertices of J: ")
-graph2.listAdjacentVertex('J')
-print("Sum of weights for the vertex J:", graph2.sumHighestAdjacentVertex('J'))
+print("\nGraph 2")
+graph2.print()
+graph2.print_adjacent_details()
