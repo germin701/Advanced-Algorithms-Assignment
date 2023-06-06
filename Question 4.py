51ee9d65aa6c761285216fd61adc742e37383aa1
germin701 <104176182+germin701@users.noreply.github.com>
2023-06-06 15:47:55 +0800

    Update Question 4.py

--- Question 4.py
+++ Question 4.py
@@ -1,11 +1,11 @@
 import threading
+import re
 
 
 class Customer:
     def __init__(self, name, amount, rate, duration):
         self.customer = name
         self.amount = amount
-        # Interest rate in percentage
         self.interest_rate = rate
         self.duration = duration
 
@@ -17,13 +17,73 @@ class Customer:
         print(f"{self.customer} Loan Amount: RM {self.amount}\n{self.customer} Interest Rate: {self.interest_rate} %\n"
               f"{self.customer} Duration: {self.duration} years\n{self.customer} Monthly Repayment: RM {payment}\n")
 
+
+# A list to store 3 customer object
 customers = []
-customer1 = Customer("John", 100000, 3.78, 5)
-customers.append(customer1)
-customer2 = Customer("Sarah", 230000, 4.22, 8)
-customers.append(customer2)
-customer3 = Customer("Rachel", 80000, 3.75, 4)
-customers.append(customer3)
+
+# A loop to ask user to input the loan details of 3 customer
+for i in range(3):
+    print("User " + str(i+1))
+    # A loop to ask the user input the name correctly
+    while True:
+        user_name = str(input("Enter the name of user " + str(i+1) + ": "))
+
+        # Validation of the username by regex
+        if not re.match("^[a-zA-Z\s'/-]+$", user_name):
+            print("Please reenter a valid name.\n")
+
+        else:
+            break
+
+    # A loop to ask user input the loan amount correctly
+    while True:
+        try:
+            loan_amount = int(input("Enter the loan amount of user " + str(i+1) + ": "))
+
+            # Validation of the loan amount
+            if loan_amount > 0:
+                break
+
+            else:
+                print("Please reenter the amount in positive number.\n")
+
+        # Error handling of the loan amount when that is not in integer
+        except ValueError:
+            print("Please reenter a valid input.\n")
+
+    # A loop to ask user input the interest rate correctly
+    while True:
+        try:
+            loan_rate = float(input("Enter the interest rate of user " + str(i+1) + "(%): "))
+
+            # Validation of the interest rate
+            if loan_rate > 0:
+                break
+
+            else:
+                print("Please reenter the interest rate in positive value.\n")
+
+        # Error handling of the interest rate when that is not in float
+        except ValueError:
+            print("Please reenter a valid input.\n")
+
+    # A loop to ask user input the loan duration correctly
+    while True:
+        try:
+            loan_duration = int(input("Enter the loan duration of user " + str(i+1) + "(year): "))
+
+            # Validation of the loan duration
+            if loan_duration > 0:
+                break
+
+            else:
+                print("Please reenter the duration in positive number.\n")
+
+        # Error handling of the loan duration when that is not in integer
+        except ValueError:
+            print("Please reenter a valid input.\n")
+    customers.append(Customer(user_name, loan_amount, loan_rate, loan_duration))
+    print()
 
 threads = []
 for customer in customers:
